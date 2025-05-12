793254b86167828241702136cb05b80de8b16d53
manojna18 <kalyani.manojna@gmail.com>
2025-05-11 22:41:45 -0400

    final commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://github.com/manojna18/bird-population-trends.git/',
+  base: '/bird-population-trends/',
   plugins: [svelte()],
   resolve: {
     alias: {
