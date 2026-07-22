37e7abfab0020d733ca23373b5a4a02c59d3ee9a
Manogna Maddipatla <momo@Manognas-MacBook-Air.local>
2026-07-22 14:21:59 -0400

    updated packaging

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,22 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-import path from 'path'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/bird-population-trends2/',
+  base: "/bird-population-trends/",
   plugins: [svelte()],
   resolve: {
     alias: {
-      "$components": path.resolve('./src/components'),
+      "$components": path.resolve("./src/components"),
       "$data": path.resolve("./src/data"),
       "$routes": path.resolve("./src/routes"),
-    }
+    },
   },
   build: {
     rollupOptions: {
-      input: path.resolve(__dirname, 'index.html'),
+      input: path.resolve(__dirname, "index.html"),
     },
-    outDir: 'dist',
-  }
-})
-
+    outDir: "dist",
+  },
+});
